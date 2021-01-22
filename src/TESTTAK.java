8e045898ea503ddcc0f8432acac9cef80b670838
aotak—macos <ao.tak@163.com>
2021-01-22 15:01:31 +0800

    test

---
+++ src/TESTTAK.java
@@ -0,0 +1,11 @@
+public class TESTTAK {
+    public static void main(String[] args) {
+        //public 访问修饰符
+        //static 关键字
+        //void 返回类型
+        //main 方法名
+        //string 类型
+        //args 字符串数组
+        System.out.println("tak作业完成");// 输出
+    }
+}
